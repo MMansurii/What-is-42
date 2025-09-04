08f82ab06934360a6be67a6116851aa8ef54024e
MMansurii <mansurimohamad92@gmail.com>
2025-09-04 23:18:18 +0000

    Changing completely

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { VitePWA } from "vite-plugin-pwa";
 
 const getBasePath = (mode: string) =>
-  mode === "development" ? undefined : "/portfolio/";
+  mode === "development" ? undefined : "/What-is-42/";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
